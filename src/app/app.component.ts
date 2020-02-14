69511e73f67368e336f7158b8b470d8c74e0f7c2
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-02-14 16:21:34 +0530

    Http

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,27 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+
+  email:string;
+  password:string;
+  url = 'http://localhost:3000/1';
   title = 'login-angular-app';
+  
+
+  constructor(private http:HttpClient){
+    http.get(this.url).toPromise().then(
+      data =>{
+        console.log(data);
+      }
+    )
+  }
+  
+  
+  onType(){
+    this.http.get(this.url).toPromise().then(
+      data =>{
+        console.log(data);
+      }
+    );
+  }
 }
