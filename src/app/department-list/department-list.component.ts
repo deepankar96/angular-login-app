bf2e1aa6da6ef0426c01f9268aa1372569e8cae7
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-03-11 13:01:30 +0530

    session

--- src/app/department-list/department-list.component.ts
+++ src/app/department-list/department-list.component.ts
@@ -3,6 +3,7 @@ import { department } from 'src/model';
 import { DepartmentService } from '../services/department.services';
 import { Subscription } from 'rxjs';
 import { Router } from '@angular/router';
+import { CollegeLoginService } from '../services/collegeLogin.services';
 
 @Component({
   selector: 'app-department-list',
@@ -12,9 +13,14 @@ import { Router } from '@angular/router';
 export class DepartmentListComponent implements OnInit,OnDestroy {
   departments:department[] = [];
   private departmentSub:Subscription;
-  constructor(public DepartmentServices:DepartmentService,private router:Router) { }
+  constructor(public DepartmentServices:DepartmentService,
+    private router:Router,
+    public collegeLoginService:CollegeLoginService) { }
 
   ngOnInit(): void {
+    if(!this.collegeLoginService.getCollegeId()){
+      this.router.navigate(['collegeLogin'])
+    }
     this.DepartmentServices.getDepartment()
     this.departmentSub = this.DepartmentServices.getDepartmentListstner().subscribe(
       (departments:department[])=>{
