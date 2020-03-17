eb5fe792ff4d79d2d11ce189560dc5cc7e4b6d5f
deepankar96 <dipankar.sarmah1639@gmail.com>
2020-03-17 16:32:34 +0530

    department login

--- src/app/services/departmentLogin.services.ts
+++ src/app/services/departmentLogin.services.ts
@@ -14,10 +14,35 @@ export class DepartmentLoginService{
         this.token = token;
         this.collegeId = collegeId;
         this.departmentId = departmentId;
+        this.saveToken()
     }
 
     getCollegeId(){
+        const currentTimeInDate = new Date()
+        var currentTime = currentTimeInDate.getTime()
+        var expireTime = parseInt(localStorage.getItem("expiresIn"))
+        if(expireTime>=currentTime){
+        this.collegeId = localStorage.getItem("collegeId")
         return this.collegeId
+        }
     }
+    getDepartmentId(){
+        const currentTimeInDate = new Date()
+        var currentTime = currentTimeInDate.getTime()
+        var expireTime = parseInt(localStorage.getItem("expiresIn"))
+        if(expireTime>=currentTime){
+        this.departmentId = localStorage.getItem("departmentId")
+        return this.departmentId
+        }
+    }
+
+    private saveToken(){
+        localStorage.setItem("token",this.token);
+        localStorage.setItem("collegeId",this.collegeId);
+        localStorage.setItem("departmentId",this.departmentId);
+        const expireTime = new Date()
+        const expire = (expireTime.getTime() + 43200000).toString();
+        localStorage.setItem("expiresIn",expire)
+        }
 
 }
